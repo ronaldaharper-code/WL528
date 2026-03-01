25381faffb27396fa420b399059bfa6e64bfa404
ronaldaharper-code <ronald.a.harper@gmail.com>
2026-03-01 16:45:15 -0500

    Remove unsupported Sanity __experimental_actions

--- sanity/schemas/hallRental.ts
+++ sanity/schemas/hallRental.ts
@@ -4,7 +4,6 @@ export default defineType({
   name: 'hallRental',
   title: 'Hall Rental Page',
   type: 'document',
-  __experimental_actions: ['update', 'publish'],
   fields: [
     defineField({
       name: 'title',
