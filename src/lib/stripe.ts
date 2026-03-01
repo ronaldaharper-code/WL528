6574d80b6a5e95ca17c100f32d018b0659d95740
ronaldaharper-code <ronald.a.harper@gmail.com>
2026-03-01 17:16:37 -0500

    Fix Stripe apiVersion for build

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2025-2-24.acacia',
 })
 
 export const DONATION_PRESETS = [25, 50, 100, 250, 500]
