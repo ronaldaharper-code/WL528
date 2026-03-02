965ec5164fc981261db03ad12936de9e48a32ec4
ronaldaharper-code <ronald.a.harper@gmail.com>
2026-03-01 23:22:02 -0500

    Fix events crash

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,9 +1,16 @@
 export const dynamic = 'force-dynamic'
+
 import { sanityClient, QUERIES } from '@/lib/sanity'
 import { EventsViewToggle } from '@/components/events/EventsViewToggle'
 
 export default async function EventsPage() {
-  const events = await sanityClient.fetch(QUERIES.publicEvents)
+  let events: any[] = []
+
+  try {
+    events = await sanityClient.fetch(QUERIES.publicEvents)
+  } catch {
+    events = []
+  }
 
   return (
     <div className="max-w-5xl mx-auto py-12 px-4">
